00e5079c1d2ed9ac4032b0344b6d783bf2806530
Andrew Larcombe <andrew@andrewl.net>
2016-07-31 22:27:32 +0100

    Genericised build script

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,13 +3,13 @@ IFS='-' read -ra PLATFORM <<< "$PLATFORM"
 GOOS=${PLATFORM[0]}
 GOARCH=${PLATFORM[1]}
 COMPILER_ENV=""
-OUTPUT_FILENAME=wherearetheyflyingto-{$GOOS}-{$GOARCH}
+OUTPUT_FILENAME=wherearetheyflyingto-$GOOS-$GOARCH
 
-echo "Building {$GOOS} ${GOARCH} to ${OUTPUT_FILENAME}"
+echo "Building ${GOOS} ${GOARCH} to ${OUTPUT_FILENAME}"
 
 if [ "${ARCH}" = "arm" ]; then
   sudo apt-get install gcc-arm-linux-gnueabi 
   COMPILER_ENV="CC=arm-linux-gnueabi-gcc CGO_ENABLED=1"
 fi
 
-${COMPILER_ENV} GOOS=${GOOS} GOARCH=${GOARCH} go build -o ${OUTPUT_FILENAME}
+env ${COMPILER_ENV} GOOS=${GOOS} GOARCH=${GOARCH} go build -o ${OUTPUT_FILENAME}
