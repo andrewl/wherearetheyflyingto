77e489f06b3fbaa279b8549a92af49f2674817b2
Andrew Larcombe <andrew@andrew-mbp.local>
2016-07-31 17:45:22 +0100

    modified compiler name

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,7 +4,7 @@ echo "Building"
 if [ "${ARCH}" = "arm" ]; then
   sudo apt-cache search gcc-arm
   sudo apt-get install gcc-arm-linux-gnueabi 
-  env CGO_ENABLED=1 GOOS=linux GOARCH=arm go build
+  env CC=arm-linux-gnueabi-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm go build
 else
   go build
 fi
