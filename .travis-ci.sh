5c7214fbd568ebb69040a8df9ffea197aad9ec8d
Andrew Larcombe <andrew@andrew-mbp.local>
2016-07-31 14:53:22 +0100

    added custom build script

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,6 +2,8 @@
 echo "Building"
 
 if [ "${ARCH}" = "arm" ]; then
+  sudo apt-cache search gcc-arm
+  sudo apt-get install gcc-arm-linux-gnueabi 
   env CGO_ENABLED=1 GOOS=linux GOARCH=arm go build
 else
   go build
