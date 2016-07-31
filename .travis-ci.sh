4455da2c35009fad9bdfd5dae517bf5769d785fb
Andrew Larcombe <andrew@andrewl.net>
2016-07-31 22:34:55 +0100

    Genemodified cgo setting

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,14 +2,14 @@
 IFS='-' read -ra PLATFORM <<< "$PLATFORM"
 GOOS=${PLATFORM[0]}
 GOARCH=${PLATFORM[1]}
-COMPILER_ENV=""
+COMPILER_ENV="CGO_ENABLED=1"
 OUTPUT_FILENAME=wherearetheyflyingto-$GOOS-$GOARCH
 
 echo "Building ${GOOS} ${GOARCH} to ${OUTPUT_FILENAME}"
 
-if [ "${ARCH}" = "arm" ]; then
+if [ "${GOARCH}" = "arm" ]; then
   sudo apt-get install gcc-arm-linux-gnueabi 
-  COMPILER_ENV="CC=arm-linux-gnueabi-gcc CGO_ENABLED=1"
+  COMPILER_ENV="CC=arm-linux-gnueabi-gcc"
 fi
 
 env ${COMPILER_ENV} GOOS=${GOOS} GOARCH=${GOARCH} go build -o ${OUTPUT_FILENAME}
