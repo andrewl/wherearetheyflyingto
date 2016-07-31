5f644bce64542c2bb0619b9e9f5d83ad76e7b46f
Andrew Larcombe <andrew@andrew-mbp.local>
2016-07-31 18:11:38 +0100

    testing deployment

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,9 +2,8 @@
 echo "Building"
 
 if [ "${ARCH}" = "arm" ]; then
-  sudo apt-cache search gcc-arm
   sudo apt-get install gcc-arm-linux-gnueabi 
-  env CC=arm-linux-gnueabi-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm go build
+  env CC=arm-linux-gnueabi-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm go build -o wherearetheyflyingto-arm
 else
   go build
 fi
