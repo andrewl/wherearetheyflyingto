bc3cda6ce3f3fc54b7e35e572dadf13cb5bb6d1d
Andrew Larcombe <andrew@andrewl.net>
2018-02-02 21:14:07 +0000

    added download destinations

--- aws/download_destinations.sh
+++ aws/download_destinations.sh
@@ -1,6 +1,6 @@
-!#/bin/bash
+#!/bin/bash
 
 #
 # Assumes aws cli tool is setup and configured correctly
 #
-aws dynamodb query --attributes-to-get destination --table-name watft --key-conditions file://key-conditions.json
+aws dynamodb query --attributes-to-get destination altitude --table-name watft --key-conditions file://key-conditions.json
