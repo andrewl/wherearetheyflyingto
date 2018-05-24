86ead297dbbd53ac57bd7344e3859fc8648c799c
Andrew Larcombe <andrew@andrewl.net>
2018-05-24 17:41:19 +0100

    Adds filter to dynamo query in order to filer out blank destinations caused by destinations not in aiport list (usually)

--- aws/get_destinations/download_destinations.sh
+++ aws/get_destinations/download_destinations.sh
@@ -3,4 +3,4 @@
 #
 # Assumes aws cli tool is setup and configured correctly this downloads the destinations from the dynamodb table
 #
-aws dynamodb query --attributes-to-get destination altitude --table-name watft --key-conditions file://key-conditions.json
+aws dynamodb query --attributes-to-get destination altitude --table-name watft --query-filter file://query-filter.json --key-conditions file://key-conditions.json
